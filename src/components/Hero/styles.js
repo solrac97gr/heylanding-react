d2057926795e1a8340d7527b6a44d448efbda152
Carlos <carlosgarcia_97gr@hotmail.com>
2020-03-02 22:12:19 -0500

    add glow

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,7 +1,7 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { scaleU } from "../../styles/animation";
 import config from "../../config";
-//import heroimg from '../../static/images/yendo.jpeg'
+import heroimg from "../../static/images/yendo.jpeg";
 
 export const HeroContainer = styled.div`
   display: flex;
@@ -19,8 +19,9 @@ export const HeroContainer = styled.div`
     #302b63,
     #0f0c29
   ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  
 
-  padding: 150px 18%;
+  padding: 10% 18%;
   box-shadow: 0px 2px 0px rgba(0, 0, 0, 0.3);
   justify-content: flex-start;
   align-items: center;
@@ -64,18 +65,36 @@ export const SeoTitle = styled.h1`
     ${scaleU({ time: "0.5s" })};
   }
 `;
+
+const neon3 = keyframes`
+
+from {
+    text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 25px #fff, 0 0 20px #591c63, 0 0 35px #591c63, 0 0 40px #591c63, 0 0 50px #591c63, 0 0 75px #591c63;
+  }
+  to {
+    text-shadow: 0 0 2.5px #fff, 0 0 5px #fff, 0 0 7.5px #fff, 0 0 10px #591c63, 0 0 17.5px #591c63, 0 0 20px #591c63, 0 0 25px #591c63, 0 0 37.5px #591c63;
+  }
+
+`
 export const MktTitle = styled.span`
-  color: white;
+  color: #591c63;
   font-family: "Pacifico";
-  font-weight: 200;
+  font-weight: 300;
   margin: 5px 0px;
-  font-size: 3rem;
+  font-size: 4rem;
   text-shadow: 1.5px 1px 1px rgba(0, 0, 0, 0.2);
+ 
+    -webkit-animation: ${neon3} 1.5s ease-in-out infinite alternate;
+    -moz-animation: ${neon3} 1.5s ease-in-out infinite alternate;
+    animation: ${neon3} 1.5s ease-in-out infinite alternate;
+  
+  
   @media (max-width: 500px) {
     font-size: 3rem;
-    ${scaleU({ time: "0.5s" })};
+   
   }
 `;
+
 
 export const ButtonCTA = styled.a`
   color: ${config.hard};
@@ -89,7 +108,7 @@ export const ButtonCTA = styled.a`
   box-shadow: 1.5px 1px 1px rgba(0, 0, 0, 0.2);
   font-weight: 700;
   &:hover {
-    background-color: #F2C94C;
+    background-color: #f2c94c;
   }
   @media (max-width: 500px) {
     width: 100%;
